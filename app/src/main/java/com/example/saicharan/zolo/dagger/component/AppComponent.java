d526a53bfd7f01e52fab4b96e2996df500c04b1a
kantigaricharan <charan15923@gmail.com>
2017-08-10 03:46:47 +0530

    Dagger 2 implementation

---
+++ app/src/main/java/com/example/saicharan/zolo/dagger/component/AppComponent.java
@@ -0,0 +1,18 @@
+package com.example.saicharan.zolo.dagger.component;
+
+import com.example.saicharan.zolo.MyApp;
+import com.example.saicharan.zolo.dagger.module.AppModule;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by NaNi on 10/08/17.
+ */
+
+@Singleton @Component(modules = AppModule.class)
+public interface AppComponent {
+    void inject(MyApp myApp);
+   // void inject(DashboardInteractorImpl dashboardInteractorImpl);
+}
